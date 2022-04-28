e499001cf159fc6f4a03333e29fef6d0f46572bf
Brandon Johnson <brandon.johnson@Brandon-Johnson.local>
2022-04-28 21:24:22 +0200

    Deposit into Escrow

--- app/src/client.ts
+++ app/src/client.ts
@@ -3,7 +3,7 @@ const anchor = require("@project-serum/anchor");
 const provider = anchor.Provider.env();
 const wallet = provider.wallet;
 
-import { PublicKey } from "@solana/web3.js";
+import { PublicKey } from '@solana/web3.js';
 
 anchor.setProvider(provider);
 
@@ -15,12 +15,15 @@ const programId = new anchor.web3.PublicKey('AJjyLsVoEfhz7ds1ZM9RU44Zkf6bNakFC86
 
 class Functions {
     async main() {
-        if (func == 'createPool') {
-            await this.createPool();
-        } else if (func == 'updatePool') {
-            await this.updatePool();
-        } else if (func == 'readPool') {
-            await this.readPool();
+        switch (func) {
+            case 'createPool':
+                await this.createPool();
+            case 'updatePool':
+                await this.updatePool();
+            case 'readPool':
+                await this.readPool();
+            case 'deposit':
+                await this.deposit();
         }
     }
 
@@ -139,6 +142,64 @@ class Functions {
         );
 
         console.log(transaction.meta.logMessages);
+    }
+
+    async deposit() {
+        const json = require("fs").readFileSync('../target/idl/whirlpool.json', "utf8");
+
+        const idl = JSON.parse(json);
+
+        const program = new anchor.Program(idl, programId);
+
+        const poolSeeds = [
+            anchor.utils.bytes.utf8.encode("pool-account"),
+            wallet.publicKey.toBytes()
+        ];
+
+        const stateSeeds = [
+            anchor.utils.bytes.utf8.encode("state-account"),
+            wallet.publicKey.toBytes(),
+            wallet.publicKey.toBytes(),
+            // MINT
+        ];
+
+        const escrowSeeds = [
+            anchor.utils.bytes.utf8.encode("escrow-account"),
+            wallet.publicKey.toBytes()
+            // MINT
+        ];
+
+        const [ poolPDA, poolBump ] = await PublicKey.findProgramAddress(poolSeeds, program.programId);
+        const [ statePDA, stateBump ] = await PublicKey.findProgramAddress(stateSeeds, program.programId);
+        const [ escrowPDA, escrowBump ] = await PublicKey.findProgramAddress(escrowSeeds, program.programId);
+
+        const accounts = {
+            user: provider.wallet.publicKey,
+            admin: provider.wallet.publicKey,
+            donorAccount: provider.wallet.publicKey,
+            poolAccount: poolPDA,
+            stateAccount: statePDA,
+            escrowAccount: escrowPDA,
+            systemProgram: anchor.web3.SystemProgram.programId
+        };
+
+        const signature = await program.rpc.readPool(
+            // bump,
+            {
+                accounts,
+                options: { commitment: "confirmed" },
+                signers: []
+            });
+
+        let transaction = await provider.connection.getTransaction(
+            signature, { commitment: "confirmed" }
+        );
+
+        console.log(transaction.meta.logMessages);
+    }
+
+    async createMintAccount() {
+        // TODO: Create Mint Account
     }
 }
 
--- programs/whirlpool/src/lib.rs
+++ programs/whirlpool/src/lib.rs
@@ -1,4 +1,12 @@
-use anchor_lang::prelude::*;
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+extern crate core;
+
+use std::{convert::TryFrom};
+use anchor_spl::{associated_token::AssociatedToken, token::{CloseAccount, Mint, Token, TokenAccount, Transfer}};
+use anchor_lang::{prelude::*, context::CpiContext};
+
 use solana_program::{
     system_program,
     account_info::{next_account_info, AccountInfo},
@@ -6,14 +14,12 @@ use solana_program::{
     program_error::ProgramError,
     pubkey::Pubkey,
     rent::Rent,
-    sysvar::Sysvar,
 };
 
 declare_id!("AJjyLsVoEfhz7ds1ZM9RU44Zkf6bNakFC86PxXM4B7kT");
 
 #[program]
 pub mod whirlpool {
-    use std::convert::TryFrom;
     use super::*;
 
     pub fn create_pool(ctx: Context<CreatePool>, bump: u8, name: String, description: String) -> ProgramResult {
@@ -75,20 +81,66 @@ pub mod whirlpool {
 
         Ok(())
     }
+
+    pub fn deposit(ctx: Context<Deposit>, state_bump: u8, escrow_bump: u8, pool_bump: u8, token_amount: u64) -> ProgramResult {
+        let state_account = &mut ctx.accounts.state_account;
+
+        state_account.sender = ctx.accounts.user.key().clone();
+        state_account.receiver = ctx.accounts.pool_account.key().clone();
+        state_account.mint = ctx.accounts.mint.key().clone();
+        state_account.escrow = ctx.accounts.escrow_account.key().clone();
+        state_account.token_amount = token_amount;
+
+        let user_key = &ctx.accounts.user.key.to_bytes();
+        let pool_account_key = &ctx.accounts.pool_account.key().to_bytes();
+        let mint_key = &ctx.accounts.mint.key().to_bytes();
+        let state_bump_bytes = &state_bump.to_le_bytes();
+
+        let vector = vec![
+            b"state-account".as_ref(),
+            user_key,
+            pool_account_key,
+            mint_key,
+            state_bump_bytes
+        ];
+
+        let signer_seeds = vec![vector.as_slice()];
+
+        let transfer_instruction = Transfer {
+            from: ctx.accounts.user.to_account_info(),
+            to: ctx.accounts.escrow_account.to_account_info(),
+            authority: ctx.accounts.user.to_account_info()
+        };
+
+        let cpi_context= CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            transfer_instruction,
+            signer_seeds.as_slice()
+        );
+
+        anchor_spl::token::transfer(cpi_context, state_account.token_amount)?;
+
+        state_account.stage = EscrowStage::Deposited.to_u8();
+
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 #[instruction(bump:u8)]
 pub struct CreatePool<'info> {
+    #[account(mut)]
     pub admin: AccountInfo<'info>,
+
     #[account(
         init,
         payer=admin,
         space=8 + 30 + 100 + 1,
         seeds=[b"pool-account", admin.key.as_ref()],
-        bump=bump
+        bump
     )]
     pub pool_account: Account<'info, Pool>,
+
     #[account(address=system_program::ID)]
     pub system_program: Program<'info, System>,
 }
@@ -123,18 +175,88 @@ pub struct ReadPool<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+pub struct Deposit<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(mut)]
+    pub admin: AccountInfo<'info>,
+
+    #[account(mut, constraint=donor_account.mint == mint.key())]
+    pub donor_account: Account<'info, TokenAccount>,
+
+    pub mint: Account<'info, Mint>,
+
+    #[account(
+        init,
+        payer=user,
+        space=1000,
+        seeds=[b"state-account", user.key.as_ref(), pool_account.key().as_ref(), mint.key().as_ref()],
+        bump
+    )]
+    pub state_account: Account<'info, EscrowState>,
+
+    #[account(
+        mut,
+        seeds=[b"pool-account", admin.key.as_ref()],
+        bump
+    )]
+    pub pool_account: Account<'info, Pool>,
+
+    #[account(
+        init,
+        payer=user,
+        seeds=[b"escrow-account", user.key.as_ref(), mint.key().as_ref()],
+        token::mint=mint,
+        token::authority=state_account,
+        bump
+    )]
+    pub escrow_account: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token>,
+
+    pub system_program: Program<'info, System>,
+
+    pub rent: Sysvar<'info, Rent>
+}
+
 #[account]
 pub struct Pool {
     pub name: String,
     pub description: String,
     pub admin: [u8; 32],
-    pub bump: u8
+    pub bump: u8,
 }
 
-// #[error_code]
-// pub enum ArgumentValidationError {
-//     #[msg("Name may not be greater than 30 bytes")]
-//     NameTooLarge,
-//     #[msg("Description may not be greater than 100 bytes")]
-//     DescriptionTooLarge
-// }
+#[account]
+pub struct EscrowState {
+    sender: Pubkey,
+    receiver: Pubkey,
+    mint: Pubkey,
+    escrow: Pubkey,
+    token_amount: u64,
+    stage: u8
+}
+
+#[derive(Clone, Copy, PartialEq)]
+pub enum EscrowStage {
+    Deposited = 0,
+    Complete = 1,
+    Reversed = 2
+}
+
+impl EscrowStage {
+    fn to_u8(&self) -> u8 {
+       *self as u8
+    }
+
+    fn to_escrow_stage(stage: u8) -> Option<EscrowStage> {
+        match stage {
+            0 => Some(EscrowStage::Deposited),
+            1 => Some(EscrowStage::Complete),
+            2 => Some(EscrowStage::Reversed),
+            _ => None
+        }
+    }
+}
